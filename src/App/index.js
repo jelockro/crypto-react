3781345140c3957608c3173827d49d8e444668b1
Josh <jlockrow@gmail.com>
2019-02-09 12:40:52 -0600

    Adds styledComponents and css library

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import './App.css'
 import Welcome from './WelcomeMessage'
+import styled,{css} from 'styled-components'
 
-
+const MyButton = styled.div`
+  color: green;
+  ${props => props.primary && css`
+    color: palevioletred;
+  `}
+`
 class App extends Component {
   render() {
     return (
       <div>
         <Welcome/> 
+        <MyButton>Hello</MyButton>
+        <MyButton primary>Hello</MyButton>
       </div>
 
     )
