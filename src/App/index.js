b9cc6d7d60472e5a70c7b5a8233e220bc5033de0
Josh <jlockrow@gmail.com>
2019-02-10 23:28:36 -0600

    adds AppLayout Component

--- src/App/index.js
+++ src/App/index.js
@@ -2,27 +2,16 @@ import React, { Component } from 'react'
 import './App.css'
 import Welcome from './WelcomeMessage'
 import styled,{css} from 'styled-components'
+import AppLayout from './AppLayout'
 
-const MyButton = styled.div`
-  color: green;
-  ${props => props.primary && css`
-    color: palevioletred;
-  `}
-`
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`;
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <AppLayout>
         <Welcome/> 
-        <MyButton>Hello</MyButton>
-        <MyButton primary>Hello</MyButton>
-        <TomatoButton primary>Hello</TomatoButton>
-      </div>
+
+      </AppLayout>
 
     )
   }
