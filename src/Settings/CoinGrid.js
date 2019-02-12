7f5c41ed81c333186ff289eb9d96ffbb282d059e
Josh <jlockrow@gmail.com>
2019-02-11 20:52:43 -0600

    adds CoinGrid

---
+++ src/Settings/CoinGrid.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled, {css} from 'styled-components'
+import {AppContext} from '../App/AppProvider'
+
+export const CoinGridStyled = styled.div`
+  display: grid;
+
+`
+
+export default function() {
+  return (
+    <AppContext.Consumer>
+      {({coinList}) => 
+        <CoinGridStyled>
+          {Object.keys(coinList)
+            .map(coinKey => 
+              <div>{coinKey}</div>
+            )
+          }
+        </CoinGridStyled>
+      }
+    </AppContext.Consumer>
+  )
+
+}
+
---
+++ src/Settings/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import WelcomeMessage from './WelcomeMessage'
+import ConfirmButton from './ConfirmButton'
+import Page from '../Shared/Page'
+import CoinGrid from '../Settings/CoinGrid'
+
+export default function() {
+  return (
+    <Page name='settings'>
+      <WelcomeMessage />
+      <ConfirmButton />
+      <CoinGrid />
+    </Page>
+  
+    
+  ) 
+}
