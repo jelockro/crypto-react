aace676edbf33c5b042cfbc23ad7f1a3f249cef1
Josh <jlockrow@gmail.com>
2019-02-19 11:33:09 -0600

    adds CoinSpotlight to dashboard

---
+++ src/Dashboard/CoinSpotlight.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Tile} from '../Shared/Tile'
+import {AppContext} from '../App/AppProvider'
+import CoinImage from '../Shared/CoinImage'
+import styled from 'styled-components'
+
+
+const SpotlightName = styled.h2`
+  text-align: centered;
+
+`
+
+export default function() {
+  return (
+    <AppContext.Consumer>
+      {({currentFavorite, coinList}) => (
+        <Tile>
+          <SpotlightName>{coinList[currentFavorite].CoinName}</SpotlightName> 
+          <CoinImage spotlight coin={coinList[currentFavorite]}>
+
+          </CoinImage>
+        </Tile>
+      )}
+    </AppContext.Consumer>
+  )
+}
