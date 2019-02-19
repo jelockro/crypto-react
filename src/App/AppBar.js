d6c0e077c8449f7a7b50b29ba52488071e8bc604
Josh <jlockrow@gmail.com>
2019-02-19 13:00:27 -0600

    fix dashboard first commit bug

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -17,18 +17,22 @@ const ControlButtonElem = styled.div`
   ${props => props.active && css`
     /* text-shadow: 0px 0px 60px #03ff03; */
     color: red;
-  `
-  }
+  `}
+  ${props => props.hidden && css`
+    /* text-shadow: 0px 0px 60px #03ff03; */
+    display: none;
+  `}
 `
 
 
 function ControlButton({name, active}) {
   return (
   <AppContext.Consumer>  
-    {({page, setPage}) => (    
+    {({firstVisit, page, setPage}) => (    
       <ControlButtonElem 
         active={page === name}
         onClick={()=> setPage(name)}
+        hidden={firstVisit && name ==='dashboard'}
       >
         {toProperCase(name)}
       </ControlButtonElem>
